f6ccb8402e75996dafddbc80f2a3ded37e256851
Dulaj Bhagya Upananda <95066839+DULAJBHAGYA@users.noreply.github.com>
2024-03-05 10:38:41 +0530

    5th march commit

--- todolist/todos.dart
+++ todolist/todos.dart
@@ -1,13 +1,33 @@
+import 'dart:convert';
 import 'dart:io';
 
 List<String> todolist = [];
 
+void saveTasksToFile() {
+  File file = File('todolist.json');
+  List<Map<String, dynamic>> tasks = [];
+  for (int i = 0; i < todolist.length; i++) {
+    tasks.add({'id': i + 1, 'task': todolist[i]});
+  }
+  file.writeAsStringSync(jsonEncode(tasks));
+}
+
+void loadTasksFromFile() {
+  File file = File('todolist.json');
+  if (file.existsSync()) {
+    List<dynamic> tasksJson = jsonDecode(file.readAsStringSync());
+    todolist = tasksJson.map((task) => task['task'].toString()).toList();
+  }
+}
+
 void getTasks() {
+  loadTasksFromFile();
   stdout.write('Enter a task or type "done" to finish add tasks: ');
   String input = stdin.readLineSync()!;
   switch (input.toLowerCase()) {
     case 'done':
       print('Tasks entered: $todolist');
+      saveTasksToFile();
       executeAction();
       break;
     default:
@@ -28,8 +48,12 @@ void addTask(String task, [int? i]) {
 
 void displayTasks() {
   print("To do list -");
+  print("-----------------------------");
+  print("| ID | task                   |");
+  print("------------------------------");
   for (int i = 0; i < todolist.length; i++) {
-    print('${i + 1} - ${todolist[i]}');
+    print('|${i + 1}| | ${todolist[i]}|');
+    print("-------------------------------");
   }
   executeAction();
 }
@@ -41,6 +65,7 @@ void deleteTask(int i) {
     }
     String deletedTask = todolist.removeAt(i);
     print("Task deleted successfully: $deletedTask");
+    saveTasksToFile();
     executeAction();
   } catch (error) {
     print(error.toString());
@@ -51,6 +76,7 @@ void deleteTask(int i) {
 void deleteAllTasks() {
   todolist.clear();
   print("All tasks deleted.");
+  saveTasksToFile();
   executeAction();
 }
 
@@ -61,6 +87,7 @@ void updateTask(int i, String newTask) {
     }
     todolist[i] = newTask;
     print('Task at index ${i + 1} updated to "$newTask"');
+    saveTasksToFile();
     executeAction();
   } catch (error) {
     print(error.toString());
